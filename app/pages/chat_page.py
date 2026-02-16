0ff076782436c53beb03682c23f898bf87a36319
ilya_nechakhin <ilya.nechakhin@gmail.com>
2026-02-16 14:05:09 +0300

    create chat page

---
+++ app/pages/chat_page.py
@@ -0,0 +1,16 @@
+import reflex as rx
+
+from ..components.sidebar import sidebar
+from ..components.chat import chat_area
+from ..core.chat_state import ChatState
+
+def chat_page():
+    """Основная страница чата"""
+    return rx.hstack(
+        sidebar(),
+        chat_area(),
+        width="100%",
+        height="100vh",
+        spacing="0",
+        on_mount=ChatState.load_chats,
+    )
