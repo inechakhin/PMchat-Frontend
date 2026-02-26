9891ba168c2792d35814707b47e4c58c740b4189
ilya_nechakhin <ilya.nechakhin@gmail.com>
2026-02-26 18:55:40 +0300

    update back port, create reflex config for running app

--- app/core/config.py
+++ app/core/config.py
@@ -10,7 +10,7 @@ class Config(BaseSettings):
     
     # Backend
     BACKEND_HOST: str = Field("localhost", env="BACKEND_HOST")
-    BACKEND_PORT: int = Field(8008, env="BACKEND_PORT")
+    BACKEND_PORT: int = Field(8001, env="BACKEND_PORT")
     
     model_config = SettingsConfigDict(
         env_file=os.path.join(BASE_DIR, ".env"),
---
+++ rxconfig.py
@@ -0,0 +1,7 @@
+import reflex as rx
+
+config = rx.Config(
+    app_name="app",
+    api_url="http://localhost:8080",
+    backend_port=8080,
+)
